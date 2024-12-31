bf2acb2af31a1091d0f5e7a04047fb6ab91456b0
msjimc <msjimc@leeds.ac.uk>
2024-12-31 01:02:59 +0000

    Added the morse clock

--- NerdClock/Form1.Designer.vb
+++ NerdClock/Form1.Designer.vb
@@ -27,7 +27,12 @@ Partial Class Form1
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ClockTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FibonacciToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MorseCodeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.p1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'p1
@@ -35,9 +40,9 @@ Partial Class Form1
         Me.p1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.p1.Location = New System.Drawing.Point(0, 0)
+        Me.p1.Location = New System.Drawing.Point(0, 27)
         Me.p1.Name = "p1"
-        Me.p1.Size = New System.Drawing.Size(500, 309)
+        Me.p1.Size = New System.Drawing.Size(500, 282)
         Me.p1.TabIndex = 0
         Me.p1.TabStop = False
         '
@@ -49,22 +54,60 @@ Partial Class Form1
         '
         Me.Timer2.Interval = 50
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClockTypeToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(500, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ClockTypeToolStripMenuItem
+        '
+        Me.ClockTypeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FibonacciToolStripMenuItem, Me.MorseCodeToolStripMenuItem})
+        Me.ClockTypeToolStripMenuItem.Name = "ClockTypeToolStripMenuItem"
+        Me.ClockTypeToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
+        Me.ClockTypeToolStripMenuItem.Text = "Clock type"
+        '
+        'FibonacciToolStripMenuItem
+        '
+        Me.FibonacciToolStripMenuItem.Checked = True
+        Me.FibonacciToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.FibonacciToolStripMenuItem.Name = "FibonacciToolStripMenuItem"
+        Me.FibonacciToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FibonacciToolStripMenuItem.Text = "Fibonacci"
+        '
+        'MorseCodeToolStripMenuItem
+        '
+        Me.MorseCodeToolStripMenuItem.Name = "MorseCodeToolStripMenuItem"
+        Me.MorseCodeToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.MorseCodeToolStripMenuItem.Text = "Morse code"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(500, 309)
         Me.Controls.Add(Me.p1)
-        Me.MinimumSize = New System.Drawing.Size(50, 31)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MinimumSize = New System.Drawing.Size(50, 39)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.p1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents p1 As System.Windows.Forms.PictureBox
     Friend WithEvents Timer1 As System.Windows.Forms.Timer
     Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
     Friend WithEvents Timer2 As System.Windows.Forms.Timer
-
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ClockTypeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FibonacciToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MorseCodeToolStripMenuItem As ToolStripMenuItem
 End Class
--- NerdClock/Form1.vb
+++ NerdClock/Form1.vb
@@ -6,10 +6,24 @@
     Private oneA As block = Nothing
     Private oneB As block = Nothing
 
+    Private oneM() As Single = {1, 3, 3, 3, 3}
+    Private twoM() As Single = {1, 1, 3, 3, 3}
+    Private threeM() As Single = {1, 1, 1, 3, 3}
+    Private fourM() As Single = {1, 1, 1, 1, 3}
+    Private fiveM() As Single = {1, 1, 1, 1, 1}
+    Private sixM() As Single = {3, 1, 1, 1, 1}
+    Private sevenM() As Single = {3, 3, 1, 1, 1}
+    Private eightM() As Single = {3, 3, 3, 1, 1}
+    Private nineM() As Single = {3, 3, 3, 3, 1}
+    Private zeroM() As Single = {3, 3, 3, 3, 3}
+
     Private btm As Bitmap = Nothing
     Private g As Graphics = Nothing
 
     Private isReSizing As Boolean
+
+    Private clockType As Integer = 1
+
     ' Private isLoading As Boolean = True
 
     Public Sub New()
@@ -29,27 +43,144 @@
 
         Timer1.Enabled = True
 
-        'Dim aDate As Date = Now
-        'Dim intervalMinute As Integer = aDate.Minute
-        'Dim interval As Integer = (intervalMinute \ 5) * 5
-        'Timer1.Interval = 301000 - (((intervalMinute - interval) * 60 * 1000) + (aDate.Second * 1000))
-
         Timer1.Interval = 60000 - (Now.Second * 1000)
 
     End Sub
 
+
     Private Sub drawTime()
+        If clockType = 1 Then
+            Fibonacci()
+        ElseIf clockType = 2 Then
+            Morsecode()
+        End If
+    End Sub
+
+    Private Sub Morsecode()
+        Timer1.Interval = 1000
+        g.Clear(Color.White)
+        Dim currenttime As Date = Now
+        Dim hour As Integer = currenttime.Hour
+        Dim minutes As Integer = currenttime.Minute
+        Dim seconds As Integer = currenttime.Second
+
+        Dim firstHour As Integer = Tens(hour)
+        Dim secondHour As Integer = hour - (10 * firstHour)
+        Dim firstMinute As Integer = Tens(minutes)
+        Dim secondMiute As Integer = minutes - (10 * firstMinute)
+        Dim firstSecond As Integer = Tens(seconds)
+        Dim secondSecond As Integer = seconds - (10 * firstSecond)
+
+        Dim code As New List(Of Single)
+
+        code.AddRange(GetMorseCode(firstHour))
+        code.AddRange(GetMorseCode(secondHour))
+
+        code.AddRange(GetMorseCode(firstMinute))
+        code.AddRange(GetMorseCode(secondMiute))
+
+        code.AddRange(GetMorseCode(firstSecond))
+        code.AddRange(GetMorseCode(secondSecond))
+
+        Dim length As Integer = 0
+        Dim todraw((code.Count * 2) - 1) As Single
+        For index As Integer = 0 To todraw.Count - 1 Step 2
+            todraw(index) = (code(index / 2) * 1)
+            todraw(index + 1) = 1
+        Next
+
+        For index As Integer = 0 To todraw.Count - 1 Step 10
+            If index > 0 Then
+                todraw(index - 1) = 3
+            End If
+        Next
+
+        For index As Integer = 0 To todraw.Count - 1 Step 20
+            If index > 0 Then
+                todraw(index - 1) = 6
+            End If
+        Next
+
+        For index As Integer = 0 To todraw.Count - 1
+            length += todraw(index) * 3
+        Next
+
+        Dim startPoint = (p1.Width / 2) - (length / 2)
+        For index As Integer = 0 To todraw.Count - 1 Step 2
+            g.DrawRectangle(Pens.Black, New Rectangle(startPoint, (p1.Height - 10) / 2, todraw(index) * 3, 10))
+            g.FillRectangle(Brushes.Black, New Rectangle(startPoint, (p1.Height - 10) / 2, todraw(index) * 3, 10))
+            startPoint += (todraw(index) + todraw(index + 1)) * 3
+        Next
+
+        p1.Image = btm
+        Text = Now.ToLongTimeString
+    End Sub
+
+    Private Function GetMorseCode(value As Integer) As Single()
+        Dim answer() As Single = {1, 1}
+
+        Select Case value
+            Case 0
+                answer = zeroM
+            Case 1
+                answer = oneM
+            Case 2
+                answer = twoM
+            Case 3
+                answer = threeM
+            Case 4
+                answer = fourM
+            Case 5
+                answer = fiveM
+            Case 6
+                answer = sixM
+            Case 7
+                answer = sevenM
+            Case 8
+                answer = eightM
+            Case 9
+                answer = nineM
+        End Select
+
+        Return answer
+    End Function
+
+    Private Function Tens(value As Integer) As Integer
+        Dim answer = 0
+        If value > 49 Then
+            answer = 5
+        ElseIf value > 39 Then
+            answer = 4
+        ElseIf value > 29 Then
+            answer = 3
+        ElseIf value > 19 Then
+            answer = 2
+        ElseIf value > 9 Then
+            answer = 1
+        ElseIf value > 0 Then
+            answer = 0
+        Else
+            answer = 0
+        End If
+
+        Return answer
+
+    End Function
+
+    Private Sub Fibonacci()
+        Timer1.Interval = 60000
+        Text = Now.ToShortTimeString
         g.Clear(Color.White)
 
         Dim currenttime As Date = Now
         Dim hour As Integer = currenttime.Hour
         If hour > 12 Then hour -= 12
 
-        five.resetCount()
-        three.resetCount()
-        two.resetCount()
-        oneA.resetCount()
-        oneB.resetCount()
+        five.ResetCount()
+        three.ResetCount()
+        two.ResetCount()
+        oneA.ResetCount()
+        oneB.ResetCount()
 
         If hour >= 5 Then
             five.IncrementCount(block.interval.hour)
@@ -119,9 +250,7 @@
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Text = Now.ToShortTimeString
         drawTime()
-        Timer1.Interval = 60000
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -160,7 +289,22 @@
     End Sub
 
     Private Sub p1_MouseHover(sender As Object, e As EventArgs) Handles p1.MouseHover
-        ToolTip1.SetToolTip(p1, "Blue = for hours, Red for minutes and Green for both")
+        If clockType = 1 Then
+            ToolTip1.SetToolTip(p1, "Blue = for hours, Red for minutes and Green for both")
+        End If
     End Sub
 
+    Private Sub FibonacciToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FibonacciToolStripMenuItem.Click
+        clockType = 1
+        drawTime()
+        MorseCodeToolStripMenuItem.Checked = False
+        FibonacciToolStripMenuItem.Checked = True
+    End Sub
+
+    Private Sub MorseCodeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MorseCodeToolStripMenuItem.Click
+        clockType = 2
+        drawTime()
+        MorseCodeToolStripMenuItem.Checked = True
+        FibonacciToolStripMenuItem.Checked = False
+    End Sub
 End Class
